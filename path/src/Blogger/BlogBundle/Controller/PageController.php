234f7c55980f1728b06a1aa0deb46d2af7ec0243
Luis Mori <luismori@glr.pe>
2013-11-26 17:06:13 -0500

    Formulario de contacto

--- path/src/Blogger/BlogBundle/Controller/PageController.php
+++ path/src/Blogger/BlogBundle/Controller/PageController.php
@@ -4,6 +4,10 @@
 namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+// Importa el nuevo espacio de nombres
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
+
 
 class PageController extends Controller
 {
@@ -18,8 +22,26 @@ class PageController extends Controller
     }
 
     public function contactAction()
-	{
-	    return $this->render('BloggerBlogBundle:Page:contact.html.twig');
-	}
+    {
+        $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            $form->bindRequest($request);
+
+            if ($form->isValid()) {
+                // realiza alguna acción, como enviar un correo electrónico
+
+                // Redirige - Esto es importante para prevenir que el usuario
+                // reenvíe el formulario si actualiza la página
+                return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+            }
+        }
+
+        return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
     
 }
